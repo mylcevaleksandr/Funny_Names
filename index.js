7856ffef7a298b394f99e19f99879048f535a289
mylcevaleksandr <myltsevaleksandr@gmail.com>
2025-02-28 23:36:03 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import sillyName from 'sillyname';
+import {randomSuperhero} from "superheroes";
+
+$.when($.ready).then(function () {
+    const nameButton = $(".btn");
+    const output = $(".output");
+
+
+    nameButton.on("click", function () {
+        const mySillyName = sillyName();
+        const mySuperHero = randomSuperhero();
+        const funnyMessage = `Whassup, ${mySillyName}? I am your superhero ${mySuperHero}!`;
+        output.text(funnyMessage);
+    });
+});
